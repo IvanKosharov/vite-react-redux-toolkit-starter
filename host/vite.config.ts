6f74ccd7857b6cece64fc42d7d43e034dea0f74d
Ivan Kosharov <ivan.kosharov@interop.io>
2025-10-21 15:57:02 +0300

    module federation

--- host/vite.config.ts
+++ host/vite.config.ts
@@ -1,6 +1,7 @@
 import { defineConfig, loadEnv } from "vite"
 import react from "@vitejs/plugin-react"
 import { federation } from "@module-federation/vite"
+import { dependencies } from "./package.json"
 
 export default defineConfig(({ mode }) => {
   const env = loadEnv(mode, process.cwd(), "")
@@ -9,13 +10,11 @@ export default defineConfig(({ mode }) => {
     build: {
       emptyOutDir: true,
       target: "esnext",
-      minify: false,
       cssCodeSplit: false,
       modulePreload: false
     },
     preview: {
-      port: parseInt(env.VITE_PORT),
-      allowedHosts: true
+      port: parseInt(env.VITE_PORT)
     },
     server: {
       port: parseInt(env.VITE_PORT)
@@ -34,10 +33,12 @@ export default defineConfig(({ mode }) => {
         },
         shared: {
           react: {
-            singleton: true
+            singleton: true,
+            requiredVersion: dependencies.react
           },
           "react-dom": {
-            singleton: true
+            singleton: true,
+            requiredVersion: dependencies["react-dom"]
           }
         }
       })
